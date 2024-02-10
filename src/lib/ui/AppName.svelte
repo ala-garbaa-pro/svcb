01aca35f4b3ac4b63f04091c56b89975bdeff915
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-10 12:04:17 +0100

    Update AppName.svelte

--- src/lib/ui/AppName.svelte
+++ src/lib/ui/AppName.svelte
@@ -1,3 +1,3 @@
 <h1 class="overflow-hidden text-sm font-semibold truncate select-none sm:text-base p4 ">
-	<a href="/">TailwindCSS SvelteKit Clipboard</a>
+	<a href="/">svcb = <b>sv</b>elte <b>c</b>lip<b>b</b>oard</a>
 </h1>
